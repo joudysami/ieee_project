4f19ca50644a7bb4aef9d01225495d25f4a69704
joud <joudysami13@gmail.com>
2026-08-27 20:47:37 +0300

    add initial structure with abstract classes and constants for routes, icons, strings, and themes

---
+++ lib/core/constant/app_string.dart
@@ -0,0 +1,11 @@
+abstract class AppString {
+// ======== Splash =======
+ static const String splash =" static const String rememberMe";
+   // ==================== Login ====================
+  static const String rememberMe = 'Remember me';
+  static const String dontHaveAccount = "Don't have an account?";
+  static const String enterYourEmail = 'Enter your email';
+  static const String enterYourPassword = 'Enter your password';
+  static const String forgetPassword = 'Forget password?';
+
+}
